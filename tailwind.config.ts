3a634bd1aa7dc818385d37e648b092b439488047
Gabriel Kuettel <gabrielkuettel@gmail.com>
2023-12-23 16:12:12 -0700

    install tailwindcss/forms, add brand custom color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss'
+const colors = require('tailwindcss/colors')
 
 const config: Config = {
   content: [
@@ -8,13 +9,15 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: colors.cyan,
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/forms')],
 }
 export default config
